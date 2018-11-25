3e5c3aa6bf8fcf8fbf4d67fff21674d332915fa4
Jerry Eddy <jeddy1@comcast.net>
2018-11-25 10:37:45 -0500

    Finish CRUD

--- ConsumerMaster/Default.aspx.cs
+++ ConsumerMaster/Default.aspx.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -16,7 +14,7 @@ namespace ConsumerMaster
         {
             if (!this.IsPostBack)
             {
-                this.BindGrid();
+                //this.BindGrid();
             }
         }
 
@@ -44,63 +42,54 @@ namespace ConsumerMaster
             }
         }
 
-        protected void Insert(object sender, EventArgs e)
-        {
-            string Username = txtUsername.Text;
-            string Provincename = txtProvinceName.Text;
-            string Cityname = txtCityname.Text;
-            string Number = txtNumber.Text;
-            string Name = txtName.Text;
-            string ContentType = txtContentType.Text;
-            string Data = txtData.Text;
+        //protected void Insert(object sender, EventArgs e)
+        //{
+        //    string consumer_first = txtConsumerFirst.Text;
+        //    string consumer_last = txtConsumerLast.Text;
+        //    //string consumer_middle = txtConsumerMiddle.Text;
+        //    string date_of_birth = txtBirthDate.Text;
+        //    string address_line_1 = txtAddress1.Text;
+        //    string address_line_2 = txtAddress2.Text;
+        //    string city = txtCity.Text;
+        //    string state = txtState.Text;
+        //    string zip_code = txtZipCode.Text;
+        //    string identifier = txtIdentifier.Text;
+        //    string gender = txtGender.Text;
+        //    string diagnosis = txtDiagnosis.Text;
+        //    string nickname_first = txtNicknameFirst.Text;
+        //    string nickname_last = txtNicknameLast.Text;
 
-            string consumer_first = txtUserName.Text;
-            string consumer_last = txtUserName") as TextBox).Text;
-            string consumer_middle = (row.FindControl("txtUserName") as TextBox).Text;
-            string date_of_birth = (row.FindControl("txtUserName") as TextBox).Text;
-            string address_line_1 = (row.FindControl("txtUserName") as TextBox).Text;
-            string address_line_2 = (row.FindControl("txtUserName") as TextBox).Text;
-            string city = (row.FindControl("txtProvincename") as TextBox).Text;
-            string state = (row.FindControl("txtCityname") as TextBox).Text;
-            string zip_code = (row.FindControl("txtNumber") as TextBox).Text;
-            string identifier = (row.FindControl("txtName") as TextBox).Text;
-            string gender = (row.FindControl("txtContentType") as TextBox).Text;
-            string diagnosis = (row.FindControl("txtContentType") as TextBox).Text;
-            string nickname_first = (row.FindControl("txtContentType") as TextBox).Text;
-            string nickname_last = (row.FindControl("txtData") as TextBox).Text;
+        //    string constr = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
+        //    using (SqlConnection con = new SqlConnection(constr))
+        //    {
+        //        using (SqlCommand cmd = new SqlCommand("Consumers_CRUD"))
+        //        {
+        //            cmd.CommandType = CommandType.StoredProcedure;
+        //            cmd.Parameters.AddWithValue("@Action", "INSERT");
+        //            //cmd.Parameters.AddWithValue("@consumer_internal_number", consumer_internal_number);
+        //            cmd.Parameters.AddWithValue("@consumer_first", consumer_first);
+        //            cmd.Parameters.AddWithValue("@consumer_last", consumer_last);
+        //            //cmd.Parameters.AddWithValue("@consumer_middle", consumer_middle);
+        //            cmd.Parameters.AddWithValue("@date_of_birth", date_of_birth);
+        //            cmd.Parameters.AddWithValue("@address_line_1", address_line_1);
+        //            cmd.Parameters.AddWithValue("@address_line_2", address_line_2);
+        //            cmd.Parameters.AddWithValue("@city", city);
+        //            cmd.Parameters.AddWithValue("@state", state);
+        //            cmd.Parameters.AddWithValue("@zip_code", zip_code);
+        //            cmd.Parameters.AddWithValue("@identifier", identifier);
+        //            cmd.Parameters.AddWithValue("@gender", gender);
+        //            cmd.Parameters.AddWithValue("@diagnosis", diagnosis);
+        //            cmd.Parameters.AddWithValue("@nickname_first", nickname_first);
+        //            cmd.Parameters.AddWithValue("@nickname_last", nickname_last);
 
-
-            string constr = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(constr))
-            {
-                using (SqlCommand cmd = new SqlCommand("Consumers_CRUD"))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@Action", "INSERT");
-                    cmd.Parameters.AddWithValue("@consumer_internal_number", consumer_internal_number);
-                    cmd.Parameters.AddWithValue("@consumer_first", consumer_first);
-                    cmd.Parameters.AddWithValue("@consumer_last", consumer_last);
-                    cmd.Parameters.AddWithValue("@consumer_middle", consumer_middle);
-                    cmd.Parameters.AddWithValue("@date_of_birth", date_of_birth);
-                    cmd.Parameters.AddWithValue("@address_line_1", address_line_1);
-                    cmd.Parameters.AddWithValue("@address_line_2", address_line_2);
-                    cmd.Parameters.AddWithValue("@city", city);
-                    cmd.Parameters.AddWithValue("@state", state);
-                    cmd.Parameters.AddWithValue("@zip_code", zip_code);
-                    cmd.Parameters.AddWithValue("@identifier", identifier);
-                    cmd.Parameters.AddWithValue("@gender", gender);
-                    cmd.Parameters.AddWithValue("@diagnosis", diagnosis);
-                    cmd.Parameters.AddWithValue("@nickname_first", nickname_first);
-                    cmd.Parameters.AddWithValue("@nickname_last", nickname_last);
-
-                    cmd.Connection = con;
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-                }
-            }
-            this.BindGrid();
-        }
+        //            cmd.Connection = con;
+        //            con.Open();
+        //            cmd.ExecuteNonQuery();
+        //            con.Close();
+        //        }
+        //    }
+        //    this.BindGrid();
+        //}
 
         protected void OnRowEditing(object sender, GridViewEditEventArgs e)
         {
@@ -114,80 +103,80 @@ namespace ConsumerMaster
             this.BindGrid();
         }
 
-        protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
-        {
-            GridViewRow row = GridView1.Rows[e.RowIndex];
-            int consumer_internal_number = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
-            string consumer_first = (row.FindControl("txtUserName") as TextBox).Text;
-            string consumer_last = (row.FindControl("txtUserName") as TextBox).Text;
-            string consumer_middle = (row.FindControl("txtUserName") as TextBox).Text;
-            string date_of_birth = (row.FindControl("txtUserName") as TextBox).Text;
-            string address_line_1 = (row.FindControl("txtUserName") as TextBox).Text;
-            string address_line_2 = (row.FindControl("txtUserName") as TextBox).Text;
-            string city = (row.FindControl("txtProvincename") as TextBox).Text;
-            string state = (row.FindControl("txtCityname") as TextBox).Text;
-            string zip_code = (row.FindControl("txtNumber") as TextBox).Text;
-            string identifier = (row.FindControl("txtName") as TextBox).Text;
-            string gender = (row.FindControl("txtContentType") as TextBox).Text;
-            string diagnosis = (row.FindControl("txtContentType") as TextBox).Text;
-            string nickname_first = (row.FindControl("txtContentType") as TextBox).Text;
-            string nickname_last = (row.FindControl("txtData") as TextBox).Text;
+        //protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        //{
+        //    GridViewRow row = GridView1.Rows[e.RowIndex];
+        //    int consumer_internal_number = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+        //    string consumer_first = (row.FindControl("txtConsumerFirst") as TextBox).Text;
+        //    string consumer_last = (row.FindControl("txtConsumerLast") as TextBox).Text;
+        //    //string consumer_middle = (row.FindControl("txtUserName") as TextBox).Text;
+        //    string date_of_birth = (row.FindControl("txtBirthDate") as TextBox).Text;
+        //    string address_line_1 = (row.FindControl("txtAddress1") as TextBox).Text;
+        //    string address_line_2 = (row.FindControl("txtAddress2") as TextBox).Text;
+        //    string city = (row.FindControl("txtCity") as TextBox).Text;
+        //    string state = (row.FindControl("txtState") as TextBox).Text;
+        //    string zip_code = (row.FindControl("txtZipCode") as TextBox).Text;
+        //    string identifier = (row.FindControl("txtIdentifier") as TextBox).Text;
+        //    string gender = (row.FindControl("txtGender") as TextBox).Text;
+        //    string diagnosis = (row.FindControl("txtDiagnosis") as TextBox).Text;
+        //    string nickname_first = (row.FindControl("txtNicknameFirst") as TextBox).Text;
+        //    string nickname_last = (row.FindControl("txtNicknameLast") as TextBox).Text;
 
-            string constr = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(constr))
-            {
-                using (SqlCommand cmd = new SqlCommand("Consumers_CRUD"))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@Action", "UPDATE");
-                    cmd.Parameters.AddWithValue("@consumer_internal_number", consumer_internal_number);
-                    cmd.Parameters.AddWithValue("@consumer_first", consumer_first);
-                    cmd.Parameters.AddWithValue("@consumer_last", consumer_last);
-                    cmd.Parameters.AddWithValue("@consumer_middle", consumer_middle);
-                    cmd.Parameters.AddWithValue("@date_of_birth", date_of_birth);
-                    cmd.Parameters.AddWithValue("@address_line_1", address_line_1);
-                    cmd.Parameters.AddWithValue("@address_line_2", address_line_2);
-                    cmd.Parameters.AddWithValue("@city", city);
-                    cmd.Parameters.AddWithValue("@state", state);
-                    cmd.Parameters.AddWithValue("@zip_code", zip_code);
-                    cmd.Parameters.AddWithValue("@identifier", identifier);
-                    cmd.Parameters.AddWithValue("@gender", gender);
-                    cmd.Parameters.AddWithValue("@diagnosis", diagnosis);
-                    cmd.Parameters.AddWithValue("@nickname_first", nickname_first);
-                    cmd.Parameters.AddWithValue("@nickname_last", nickname_last);
+        //    string constr = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
+        //    using (SqlConnection con = new SqlConnection(constr))
+        //    {
+        //        using (SqlCommand cmd = new SqlCommand("Consumers_CRUD"))
+        //        {
+        //            cmd.CommandType = CommandType.StoredProcedure;
+        //            cmd.Parameters.AddWithValue("@Action", "UPDATE");
+        //            cmd.Parameters.AddWithValue("@consumer_internal_number", consumer_internal_number);
+        //            cmd.Parameters.AddWithValue("@consumer_first", consumer_first);
+        //            cmd.Parameters.AddWithValue("@consumer_last", consumer_last);
+        //            //cmd.Parameters.AddWithValue("@consumer_middle", consumer_middle);
+        //            cmd.Parameters.AddWithValue("@date_of_birth", date_of_birth);
+        //            cmd.Parameters.AddWithValue("@address_line_1", address_line_1);
+        //            cmd.Parameters.AddWithValue("@address_line_2", address_line_2);
+        //            cmd.Parameters.AddWithValue("@city", city);
+        //            cmd.Parameters.AddWithValue("@state", state);
+        //            cmd.Parameters.AddWithValue("@zip_code", zip_code);
+        //            cmd.Parameters.AddWithValue("@identifier", identifier);
+        //            cmd.Parameters.AddWithValue("@gender", gender);
+        //            cmd.Parameters.AddWithValue("@diagnosis", diagnosis);
+        //            cmd.Parameters.AddWithValue("@nickname_first", nickname_first);
+        //            cmd.Parameters.AddWithValue("@nickname_last", nickname_last);
 
-                    //cmd.Parameters.AddWithValue("@ContentType", SqlDbType.VarBinary, -1);
-                    //cmd.Parameters.AddWithValue("@Data", SqlDbType.VarBinary, -1);
+        //            //cmd.Parameters.AddWithValue("@ContentType", SqlDbType.VarBinary, -1);
+        //            //cmd.Parameters.AddWithValue("@Data", SqlDbType.VarBinary, -1);
 
-                    cmd.Connection = con;
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-                }
-            }
-            GridView1.EditIndex = -1;
-            this.BindGrid();
-        }
+        //            cmd.Connection = con;
+        //            con.Open();
+        //            cmd.ExecuteNonQuery();
+        //            con.Close();
+        //        }
+        //    }
+        //    GridView1.EditIndex = -1;
+        //    this.BindGrid();
+        //}
 
-        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
-        {
-            int consumer_internal_number = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
-            string constr = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(constr))
-            {
-                using (SqlCommand cmd = new SqlCommand("Consumers_CRUD"))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@Action", "DELETE");
-                    cmd.Parameters.AddWithValue("@consumer_internal_number",consumer_internal_number);
-                    cmd.Connection = con;
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-                }
-            }
-            this.BindGrid();
-        }
+        //protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        //{
+        //    int consumer_internal_number = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+        //    string constr = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
+        //    using (SqlConnection con = new SqlConnection(constr))
+        //    {
+        //        using (SqlCommand cmd = new SqlCommand("Consumers_CRUD"))
+        //        {
+        //            cmd.CommandType = CommandType.StoredProcedure;
+        //            cmd.Parameters.AddWithValue("@Action", "DELETE");
+        //            cmd.Parameters.AddWithValue("@consumer_internal_number",consumer_internal_number);
+        //            cmd.Connection = con;
+        //            con.Open();
+        //            cmd.ExecuteNonQuery();
+        //            con.Close();
+        //        }
+        //    }
+        //    this.BindGrid();
+        //}
 
         protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
         {
