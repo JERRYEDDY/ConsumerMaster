080d146a9037f4f6f425fa63f7bd5e84d97ff5f8
jeddy yourpathways.org <jeddy@yourpathways.org>
2019-06-05 16:02:15 -0400

    Clean up

--- ConsumerMaster/Default.aspx.cs
+++ ConsumerMaster/Default.aspx.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Web.UI;
-using Telerik.Windows.Documents.Spreadsheet.Model;
-using System.Data.SqlClient;
-using System.Data;
-using System.Configuration;
-using Telerik.Web.UI;
 
 namespace ConsumerMaster
 {
@@ -17,66 +12,6 @@ namespace ConsumerMaster
             if (!this.IsPostBack)
             {
                 Logger.Info("ConsumerMaster started");
-                //BindToTPDropDownList(TPRadDropDownList);
-            }
-        }
-
-        protected void ConsumerExportDownload_Click(object sender, EventArgs e)
-        {
-            const string filename = @"ConsumerExport.xlsx";
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-            }
-        }
-
-        protected void EIServiceExportDownload_Click(object sender, EventArgs e)
-        {
-            const string filename = @"EIServiceExport.xlsx";
-            try
-            {
-                EIServiceExportExcelFileReadWrite serviceExport = new EIServiceExportExcelFileReadWrite();
-                Workbook workbook = serviceExport.CreateWorkbook();
-                Utility utility = new Utility();
-                utility.DownloadExcelFile(workbook, filename);
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-            }
-        }
-
-        private void BindToTPDropDownList(RadDropDownList dropdownlist)
-        {
-            try
-            {
-                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnStringDb1"].ToString()))
-                {
-                    con.Open();
-                    using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT id AS trading_partner_id, name FROM TradingPartners", con))
-                    {
-                        DataTable tradingPartners = new DataTable();
-                        adapter.Fill(tradingPartners);
-
-                        DataRow dr = tradingPartners.NewRow();
-                        dr["trading_partner_id"] = "0";
-                        dr["name"] = "ALL TRADING PARTNERS";
-                        tradingPartners.Rows.InsertAt(dr, 0);
-
-                        dropdownlist.DataTextField = "name";
-                        dropdownlist.DataValueField = "trading_partner_id";
-                        dropdownlist.DataSource = tradingPartners;
-                        dropdownlist.DataBind();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
             }
         }
     }
