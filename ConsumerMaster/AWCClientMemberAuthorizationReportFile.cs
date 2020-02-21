917afa83cbdce83e0403596ec8ccbab83ca2e8b7
jeddy yourpathways.org <jeddy@yourpathways.org>
2020-02-21 14:25:12 -0500

    Working on Data Insight reports used for input to make sure they have all active AWC clients

--- ConsumerMaster/AWCClientMemberAuthorizationReportFile.cs
+++ ConsumerMaster/AWCClientMemberAuthorizationReportFile.cs
@@ -84,37 +84,41 @@ namespace ConsumerMaster
 
 
 
-                var LeftJoin = from dt1row in clients.AsEnumerable()
-                               join dt2row in members.AsEnumerable() on dt1row.Field<string>("ClientID") equals dt2row.Field<string>("ClientID") into table3
-                               from t3row in table3.DefaultIfEmpty()
-                               select new
-                               {
-                                   CLientID = dt1row.Field<string>("ClientID"),
-                                   Matched = t3row != null ? "Yes" : "NO"
-                               };
+                //var LeftJoin = from dt1row in clients.AsEnumerable()
+                //               join dt2row in members.AsEnumerable() on dt1row.Field<string>("ClientID") equals dt2row.Field<string>("ClientID") into table3
+                //               from t3row in table3.DefaultIfEmpty()
+                //               select new
+                //               {
+                //                   CLientID = dt1row.Field<string>("ClientID"),
+                //                   Matched = t3row != null ? "Yes" : "NO"
+                //               };
 
 
 
 
 
-                //joining Client and Member DataTable   
-                var JoinResult = (from c in clients.AsEnumerable()
-                                  join m in members.AsEnumerable()
-                                  on c.Field<string>("ClientID") equals m.Field<string>("ClientID")
-                                  into temp
-                                  from m in temp.DefaultIfEmpty()
-                                  select new
-                                  {
-                                      ClientCID = c.Field<string>("ClientID"),
-                                      MemberCID = m.Field<string>("ClientID")
-                                  }).ToList();
-
-
+                ////joining Client and Member DataTable   
+                //var JoinResult = (from c in clients.AsEnumerable()
+                //                  join m in members.AsEnumerable()
+                //                  on c.Field<string>("ClientID") equals m.Field<string>("ClientID")
+                //                  into temp
+                //                  from m in temp.DefaultIfEmpty()
+                //                  select new
+                //                  {
+                //                      ClientCID = c.Field<string>("ClientID"),
+                //                      MemberCID = m.Field<string>("ClientID")
+                //                  }).ToList();
 
 
                 DataSet clientStaffAuthorizations = new DataSet("ClientStaffAuthorizations");
+
+                clients.PrimaryKey = new DataColumn[] { clients.Columns["ClientID"] };
                 clientStaffAuthorizations.Tables.Add(clients);
+
+                //members.PrimaryKey = new DataColumn[] { members.Columns["ClientID"] };
                 clientStaffAuthorizations.Tables.Add(members);
+
+                //authorizations.PrimaryKey = new DataColumn[] { authorizations.Columns["ClientID"] };
                 clientStaffAuthorizations.Tables.Add(authorizations);
 
                 clientStaffAuthorizations.Relations.Add(memberRangeName, clients.Columns["ClientID"], members.Columns["ClientID"], false);
