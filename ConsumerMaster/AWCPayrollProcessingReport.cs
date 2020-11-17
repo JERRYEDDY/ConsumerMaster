2ced8109abf9b61332799fc8bce422d8e2adade2
jeddy yourpathways.org <jeddy@yourpathways.org>
2020-11-17 14:05:01 -0500

    Added dropdowns to Payroll Processing File excel

--- ConsumerMaster/AWCPayrollProcessingReport.cs
+++ ConsumerMaster/AWCPayrollProcessingReport.cs
@@ -16,27 +16,50 @@ namespace ConsumerMaster
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         private static readonly int IndexRowItemStart = 0;
 
-        private readonly SortedList<string, bool> headerColumns = new SortedList<string, bool>()
+        private readonly HeaderColumns[] hdrColumns = new HeaderColumns[]
         {
-            {"Client_ID", false },
-            {"Client_Name", false },
-            {"Staff Name", false },
-            {"Num_of_IVR_Tranactions", false },
-            {"Num_of Mobile_Tranactions", false },
-            {"Num_of Portal_Tranactions", false },
-            {"Progress Notes Week 1?", true },
-            {"Progress Notes Week 2?", true },
-            {"Notes_PN", false },
-            {"Progress Notes Initials", true },
-            {"Notes_Evolv Submission and Payroll", false },
-            {"Submit initials", true },
-            {"Evolv Submit", true },
-            {"Evolv Approval", true },
-            {"Sent for Payroll", true },
-            {"Evolv Submit (original)", true },
-            {"Evolv Approval (orginal)", true },
-            {"Sent for Payroll (original)", true },
+            new HeaderColumns(0, "Client_ID", false),
+            new HeaderColumns(1, "Client_Name", false),
+            new HeaderColumns(2, "Staff Name", false),
+            new HeaderColumns(3, "Num_of_IVR_Tranactions", false),
+            new HeaderColumns(4, "Num_of Mobile_Tranactions", false),
+            new HeaderColumns(5, "Num_of Portal_Tranactions", false),
+            new HeaderColumns(6, "Progress Notes Week 1?", true),
+            new HeaderColumns(7, "Progress Notes Week 2?", true),
+            new HeaderColumns(8, "Notes_PN             ", false),
+            new HeaderColumns(9, "Progress Notes Initials", true),
+            new HeaderColumns(10, "Notes_Evolv Submission and Payroll", false),
+            new HeaderColumns(11, "Submit initials", true),
+            new HeaderColumns(12, "Evolv Submit", true),
+            new HeaderColumns(13, "Evolv Approval", true),
+            new HeaderColumns(14, "Sent for Payroll", true),
+            new HeaderColumns(15, "Evolv Submit (original)", true),
+            new HeaderColumns(16, "Evolv Approval (orginal)", true),
+            new HeaderColumns(17, "Sent for Payroll (original)", true)
         };
+
+
+        public static class Header
+        {
+            public const int Client_ID = 0;
+            public const int Client_Name = 1;
+            public const int Staff_Name = 2;
+            public const int Num_of_IVR_Tranactions = 3;
+            public const int Num_of_Mobile_Tranactions = 4;
+            public const int Num_of_Portal_Tranactions = 5;
+            public const int Progress_Notes_Week_1 = 6;
+            public const int Progress_Notes_Week_2 = 7;
+            public const int Notes_PN = 8;
+            public const int Progress_Notes_Initials = 9;
+            public const int Notes_Evolv_Submission_and_Payroll = 10;
+            public const int Submit_initials = 11;
+            public const int Evolv_Submit = 12;
+            public const int Evolv_Approval = 13;
+            public const int Sent_for_Payroll = 14;
+            public const int Evolv_Submit_original = 15;
+            public const int Evolv_Approval_orginal = 16;
+            public const int Sent_for_Payroll_original = 17;
+        }
 
         public Workbook CreateWorkbook(UploadedFile uploadedFile)
         {
@@ -108,47 +131,112 @@ namespace ConsumerMaster
 
                 PrepareSheet1Worksheet(sheet1Worksheet);
 
+
+        //    public const int Client_ID = 0;
+        //public const int Client_Name = 1;
+        //public const int Staff_Name = 2;
+        //public const int Num_of_IVR_Tranactions = 3;
+        //public const int Num_of_Mobile_Tranactions = 4;
+        //public const int Num_of_Portal_Tranactions = 5;
+        //public const int Progress_Notes_Week_1 = 6;
+        //public const int Progress_Notes_WeeK_2 = 7;
+        //public const int Notes_PN = 8;
+        //public const int Progress_Notes_Initials = 9;
+        //public const int Notes_Evolv_Submission_and_Payroll = 10;
+        //public const int Submit_initials = 11;
+        //public const int Evolv_Submit = 12;
+        //public const int Evolv_Approval = 13;
+        //public const int Sent_for_Payroll = 14;
+        //public const int Evolv_Submit_original = 15;
+        //public const int Evolv_Approval_orginal = 16;
+        //public const int Sent_for_Payroll_original = 17;
+
+
                 int currentRow = IndexRowItemStart + 1;
                 foreach (DataRow row in dTable.Rows)
                 {
                     int column = 0;
 
-                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["ID"].ToString());
-                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Name"].ToString());
-                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["StaffName"].ToString());
+                    sheet1Worksheet.Cells[currentRow, Header.Client_ID].SetValue(row["ID"].ToString());
+                    sheet1Worksheet.Cells[currentRow, Header.Client_Name].SetValue(row["Name"].ToString());
+                    sheet1Worksheet.Cells[currentRow, Header.Staff_Name].SetValue(row["StaffName"].ToString());
 
-                    //CellValueFormat dateCellValueFormat = new CellValueFormat("MM/dd/yyyy hh:mm AM/PM");
-                    //sheet1Worksheet.Cells[currentRow, column].SetFormat(dateCellValueFormat);
-                    //sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Start"].ToString());
+                    sheet1Worksheet.Cells[currentRow, Header.Num_of_IVR_Tranactions].SetValue(row["ICount"].ToString()); //Num_of_IVR_Tranactions
+                    sheet1Worksheet.Cells[currentRow, Header.Num_of_Mobile_Tranactions].SetValue(row["MCount"].ToString());
+                    sheet1Worksheet.Cells[currentRow, Header.Num_of_Portal_Tranactions].SetValue(row["PCount"].ToString());
 
-                    //sheet1Worksheet.Cells[currentRow, column].SetFormat(dateCellValueFormat);
-                    //sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Finish"].ToString());
+                    CellIndex c1RuleCellIndex = new CellIndex(currentRow, Header.Progress_Notes_Week_1);
+                    ListDataValidationRuleContext c1Context = new ListDataValidationRuleContext(sheet1Worksheet, c1RuleCellIndex);
+                    c1Context.InCellDropdown = true;
+                    c1Context.Argument1 = "Yes";
+                    ListDataValidationRule c1Rule = new ListDataValidationRule(c1Context);
+                    sheet1Worksheet.Cells[c1RuleCellIndex].SetDataValidationRule(c1Rule);
+                    sheet1Worksheet.Cells[currentRow, Header.Progress_Notes_Week_1].SetValue(""); //Progress Notes Week1
 
-                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["ICount"].ToString());
-                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["MCount"].ToString());
-                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["PCount"].ToString());
+                    CellIndex c2RuleCellIndex = new CellIndex(currentRow, Header.Progress_Notes_Week_2);
+                    ListDataValidationRuleContext c2Context = new ListDataValidationRuleContext(sheet1Worksheet,c2RuleCellIndex);
+                    c2Context.InCellDropdown = true;
+                    c2Context.Argument1 = "Yes";
+                    ListDataValidationRule c2Rule = new ListDataValidationRule(c2Context);
+                    sheet1Worksheet.Cells[c2RuleCellIndex].SetDataValidationRule(c2Rule);
+                    sheet1Worksheet.Cells[currentRow, Header.Progress_Notes_Week_2].SetValue(""); //Progress Notes Week2
 
-                    CellIndex week1RuleCellIndex = new CellIndex(currentRow, column);
-                    ListDataValidationRuleContext week1Context = new ListDataValidationRuleContext(sheet1Worksheet, week1RuleCellIndex);
-                    week1Context.InCellDropdown = true;
-                    week1Context.Argument1 = "Yes";
-                    ListDataValidationRule week1Rule = new ListDataValidationRule(week1Context);
-                    sheet1Worksheet.Cells[week1RuleCellIndex].SetDataValidationRule(week1Rule);
-
-                    int currentColumn = headerColumns.IndexOfKey("Progress Notes Week 1?");
-                    
-
-
-                    sheet1Worksheet.Cells[currentRow, column++].SetValue(""); //Progress Notes Week1
+        //    public const int Evolv_Submit = 12;
+        //public const int Evolv_Approval = 13;
+        //public const int Sent_for_Payroll = 14;
+        //public const int Evolv_Submit_original = 15;
+        //public const int Evolv_Approval_orginal = 16;
+        //public const int Sent_for_Payroll_original = 17;
 
 
-                    sheet1Worksheet.Cells[currentRow, column++].SetValue(""); //Progress Notes Week2
 
-                    //ThemableColor textColor = new ThemableColor(Colors.Red);
-                    //sheet1Worksheet.Cells[currentRow, column].SetForeColor(textColor);
-                    //sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Exception"].ToString());
+                    CellIndex c3RuleCellIndex = new CellIndex(currentRow, Header.Evolv_Submit);
+                    ListDataValidationRuleContext c3Context = new ListDataValidationRuleContext(sheet1Worksheet, c3RuleCellIndex);
+                    c3Context.InCellDropdown = true;
+                    c3Context.Argument1 = "Done";
+                    ListDataValidationRule c3Rule = new ListDataValidationRule(c3Context);
+                    sheet1Worksheet.Cells[c3RuleCellIndex].SetDataValidationRule(c3Rule);
+                    sheet1Worksheet.Cells[currentRow, Header.Evolv_Submit].SetValue("");
 
-                    //sheet1Worksheet.Cells[currentRow, column++].SetValue(row["NS Billing Auth"].ToString());
+                    CellIndex c4RuleCellIndex = new CellIndex(currentRow, Header.Evolv_Approval);
+                    ListDataValidationRuleContext c4Context = new ListDataValidationRuleContext(sheet1Worksheet, c4RuleCellIndex);
+                    c4Context.InCellDropdown = true;
+                    c4Context.Argument1 = "Done";
+                    ListDataValidationRule c4Rule = new ListDataValidationRule(c4Context);
+                    sheet1Worksheet.Cells[c4RuleCellIndex].SetDataValidationRule(c4Rule);
+                    sheet1Worksheet.Cells[currentRow, Header.Evolv_Approval].SetValue("");
+
+                    CellIndex c5RuleCellIndex = new CellIndex(currentRow, Header.Sent_for_Payroll);
+                    ListDataValidationRuleContext c5Context = new ListDataValidationRuleContext(sheet1Worksheet, c5RuleCellIndex);
+                    c5Context.InCellDropdown = true;
+                    c5Context.Argument1 = "Done";
+                    ListDataValidationRule c5Rule = new ListDataValidationRule(c5Context);
+                    sheet1Worksheet.Cells[c5RuleCellIndex].SetDataValidationRule(c5Rule);
+                    sheet1Worksheet.Cells[currentRow, Header.Sent_for_Payroll].SetValue("");
+
+                    CellIndex c6RuleCellIndex = new CellIndex(currentRow, Header.Evolv_Submit_original);
+                    ListDataValidationRuleContext c6Context = new ListDataValidationRuleContext(sheet1Worksheet, c6RuleCellIndex);
+                    c6Context.InCellDropdown = true;
+                    c6Context.Argument1 = "Done";
+                    ListDataValidationRule c6Rule = new ListDataValidationRule(c6Context);
+                    sheet1Worksheet.Cells[c6RuleCellIndex].SetDataValidationRule(c6Rule);
+                    sheet1Worksheet.Cells[currentRow, Header.Evolv_Submit_original].SetValue("");
+
+                    CellIndex c7RuleCellIndex = new CellIndex(currentRow, Header.Evolv_Approval_orginal);
+                    ListDataValidationRuleContext c7Context = new ListDataValidationRuleContext(sheet1Worksheet, c7RuleCellIndex);
+                    c7Context.InCellDropdown = true;
+                    c7Context.Argument1 = "Done";
+                    ListDataValidationRule c7Rule = new ListDataValidationRule(c7Context);
+                    sheet1Worksheet.Cells[c7RuleCellIndex].SetDataValidationRule(c7Rule);
+                    sheet1Worksheet.Cells[currentRow, Header.Evolv_Approval_orginal].SetValue("");
+
+                    CellIndex c8RuleCellIndex = new CellIndex(currentRow, Header.Sent_for_Payroll_original);
+                    ListDataValidationRuleContext c8Context = new ListDataValidationRuleContext(sheet1Worksheet, c8RuleCellIndex);
+                    c8Context.InCellDropdown = true;
+                    c8Context.Argument1 = "Done";
+                    ListDataValidationRule c8Rule = new ListDataValidationRule(c8Context);
+                    sheet1Worksheet.Cells[c8RuleCellIndex].SetDataValidationRule(c8Rule);
+                    sheet1Worksheet.Cells[currentRow, Header.Sent_for_Payroll_original].SetValue("");
 
                     currentRow++;
                 }
@@ -168,6 +256,26 @@ namespace ConsumerMaster
 
         private void PrepareSheet1Worksheet(Worksheet worksheet)
         {
+            //HeaderColumns[] hdrColumns = new HeaderColumns[18];
+            //hdrColumns[0] = new HeaderColumns("Client_ID", false);
+            //hdrColumns[1] = new HeaderColumns("Client_Name", false);
+            //hdrColumns[2] = new HeaderColumns("Staff Name", false);
+            //hdrColumns[3] = new HeaderColumns("Num_of_IVR_Tranactions", false);
+            //hdrColumns[4] = new HeaderColumns("Num_of Mobile_Tranactions", false);
+            //hdrColumns[5] = new HeaderColumns("Num_of Portal_Tranactions", false);
+            //hdrColumns[6] = new HeaderColumns("Progress Notes Week 1?", true);
+            //hdrColumns[7] = new HeaderColumns("Progress Notes Week 2?", true);
+            //hdrColumns[8] = new HeaderColumns("Notes_PN             ", false);
+            //hdrColumns[9] = new HeaderColumns("Progress Notes Initials", true);
+            //hdrColumns[10] = new HeaderColumns("Notes_Evolv Submission and Payroll", false);
+            //hdrColumns[11] = new HeaderColumns("Submit initials", true);
+            //hdrColumns[12] = new HeaderColumns("Evolv Submit", true);
+            //hdrColumns[13] = new HeaderColumns("Evolv Approval", true);
+            //hdrColumns[14] = new HeaderColumns("Sent for Payroll", true);
+            //hdrColumns[15] = new HeaderColumns("Evolv Submit (original)", true);
+            //hdrColumns[16] = new HeaderColumns("Evolv Approval (orginal)", true);
+            //hdrColumns[17] = new HeaderColumns("Sent for Payroll (original)", true);
+
             try
             {
                 PatternFill solidPatternFill = new PatternFill(PatternType.Solid, Color.FromArgb(255, 192, 192, 192), Colors.Transparent);
@@ -175,11 +283,11 @@ namespace ConsumerMaster
                 worksheet.ViewState.FreezePanes(0, 3);
 
                 int columnKey = 0;
-                foreach (KeyValuePair<string, bool> header in headerColumns)
+                foreach (HeaderColumns header in hdrColumns)
                 {
-                    string columnName = header.Key;
-                    bool colValue = header.Value;
-                    worksheet.Cells[IndexRowItemStart, columnKey].SetIsWrapped(colValue);
+                    string columnName = header.Name;
+                    bool iswrap = header.IsWrap;
+                    worksheet.Cells[IndexRowItemStart, columnKey].SetIsWrapped(iswrap);
                     worksheet.Cells[IndexRowItemStart, columnKey].SetValue(columnName);
 
                     if (columnKey < 6)
@@ -209,6 +317,23 @@ namespace ConsumerMaster
             }
             return table;
         }
+
+
+        public class HeaderColumns 
+        {
+            public int Col { get; set; }
+            public string Name { get; set; }
+            public bool IsWrap { get; set; }
+
+            public HeaderColumns(int col, string name, bool iswrap )
+            {
+                Col = col;
+                Name = name;
+                IsWrap = iswrap;
+            }
+
+        }
+
 
         public class PayrollProcessClient
         {
