db68d89468e3e661449bfd597fcda5b656671dab
JERRYEDDY <jeddy@yourpathways.org>
2021-01-25 15:29:34 -0500

    Add MISMATCHED to EVVVisitsComparison Report

--- ConsumerMaster/AWCEVVVisitsComparisonReportFile.cs
+++ ConsumerMaster/AWCEVVVisitsComparisonReportFile.cs
@@ -14,74 +14,74 @@ namespace ConsumerMaster
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         private static readonly int IndexRowItemStart = 0;
 
-        string[] billingCodeArray = new string[13]
-        {
-            "ODP / W1726 / Companion W/B",
-            "ODP / W1726:U4 / Companion W/O",
-            "ODP/ W7061 / H&C 1:1 Degreed Staff",
-            "ODP / W7060 / H&C 1:1 W/B",
-            "ODP / W7060:U4 / H&C 1:1 W/O",
-            "ODP / W7069 / H&C 2:1 Enhanced W/B",
-            "ODP / W7068 / H&C 2:1 W/B",
-            "ODP / W9863 / Respite 1:1 Enhanced 15 min W/B",
-            "ODP / W9862 / Respite 15 min W/B",
-            "ODP / W9862:U4 / Respite 15 min W/O",
-            "ODP / W9798 / Respite 24HR W/B",
-            "ODP / W9798:U4/ Respite 24 HR W/O",
-            "Supported Employment (All)"
-        };
+        //string[] billingCodeArray = new string[13]
+        //{
+        //    "ODP / W1726 / Companion W/B",
+        //    "ODP / W1726:U4 / Companion W/O",
+        //    "ODP/ W7061 / H&C 1:1 Degreed Staff",
+        //    "ODP / W7060 / H&C 1:1 W/B",
+        //    "ODP / W7060:U4 / H&C 1:1 W/O",
+        //    "ODP / W7069 / H&C 2:1 Enhanced W/B",
+        //    "ODP / W7068 / H&C 2:1 W/B",
+        //    "ODP / W9863 / Respite 1:1 Enhanced 15 min W/B",
+        //    "ODP / W9862 / Respite 15 min W/B",
+        //    "ODP / W9862:U4 / Respite 15 min W/O",
+        //    "ODP / W9798 / Respite 24HR W/B",
+        //    "ODP / W9798:U4/ Respite 24 HR W/O",
+        //    "Supported Employment (All)"
+        //};
 
-        string[] sandataServiceArray = new string[8]
-        {
-            "Companion (1:1)",
-            "IHCS Level 2 (1:1)",
-            "IHCS Level 2 (1:1) Enhanced",
-            "IHCS Level 3 (2:1)",
-            "IHCS Level 3 (2:1) Enhanced",
-            "Respite Level 3 (1:1)-Day",
-            "Respite Level 3 (1:1)-15 Mins",
-            "Respite Level 3 (1:1) Enhanced-15 Mins"
-        };
+        //string[] sandataServiceArray = new string[8]
+        //{
+        //    "Companion (1:1)",
+        //    "IHCS Level 2 (1:1)",
+        //    "IHCS Level 2 (1:1) Enhanced",
+        //    "IHCS Level 3 (2:1)",
+        //    "IHCS Level 3 (2:1) Enhanced",
+        //    "Respite Level 3 (1:1)-Day",
+        //    "Respite Level 3 (1:1)-15 Mins",
+        //    "Respite Level 3 (1:1) Enhanced-15 Mins"
+        //};
 
-        string[] payrollCodeArray = new string[16]
-        {
-            "Companion W/B (W1726)",
-            "Companion W/O (W1726:U4)",
-            "H&C 1:1 Degreed Staff (W7061)",
-            "H&C 1:1 W/B (W7060)",
-            "H&C 1:1 W/O (W7060:U4)",
-            "H&C 2:1 Enhanced W/B (W7069)",
-            "H&C 2:1 W/B (W7068)",
-            "Respite 1:1 Enhanced 15 min W/B (W9863)",
-            "Respite 15 min W/B (W9862)",
-            "Respite 15 min W/O (W9862:U4)",
-            "Respite 24HR W/B (W9798)",
-            "Respite 24 HR W/O (W9798:U4)",
-            "SE Career Assessment W/B (W7235)",
-            "SE Job Coach W/B (W9794)",
-            "SE Job Coach W/O (W9794:U4)",
-            "SE Job Find W/B (H2023)"
-        };
+        //string[] payrollCodeArray = new string[16]
+        //{
+        //    "Companion W/B (W1726)",
+        //    "Companion W/O (W1726:U4)",
+        //    "H&C 1:1 Degreed Staff (W7061)",
+        //    "H&C 1:1 W/B (W7060)",
+        //    "H&C 1:1 W/O (W7060:U4)",
+        //    "H&C 2:1 Enhanced W/B (W7069)",
+        //    "H&C 2:1 W/B (W7068)",
+        //    "Respite 1:1 Enhanced 15 min W/B (W9863)",
+        //    "Respite 15 min W/B (W9862)",
+        //    "Respite 15 min W/O (W9862:U4)",
+        //    "Respite 24HR W/B (W9798)",
+        //    "Respite 24 HR W/O (W9798:U4)",
+        //    "SE Career Assessment W/B (W7235)",
+        //    "SE Job Coach W/B (W9794)",
+        //    "SE Job Coach W/O (W9794:U4)",
+        //    "SE Job Find W/B (H2023)"
+        //};
 
-        string[] serviceCodeArray = new string[16]
-        {
-            "(W1726): Companion W/B",
-            "(W1726:U4): Companion W/O",
-            "(W7061): H&C 1:1 Degreed Staff",
-            "(W7060): H&C 1:1 W/B",
-            "(W7060:U4): H&C 1:1 W/O",
-            "(W7069): H&C 2:1 Enhanced W/B",
-            "(W7068): H&C 2:1 W/B",
-            "(W9863):Respite 1:1 Enhanced 15 min W/B",
-            "(W9862):Respite 15 min W/B",
-            "(W9862:U4):Respite 15 min W/O",
-            "(W9798):Respite 24HR W/B",
-            "(W9798:U4):Respite 24 HR W/O",
-            "(W7235):SE Career Assessment W/B",
-            "(W9794): SE Job Coach W/B",
-            "(W9794:U4):SE Job Coach W/O",
-            "(H2023):SE Job Find W/B"
-        };
+        //string[] serviceCodeArray = new string[16]
+        //{
+        //    "(W1726): Companion W/B",
+        //    "(W1726:U4): Companion W/O",
+        //    "(W7061): H&C 1:1 Degreed Staff",
+        //    "(W7060): H&C 1:1 W/B",
+        //    "(W7060:U4): H&C 1:1 W/O",
+        //    "(W7069): H&C 2:1 Enhanced W/B",
+        //    "(W7068): H&C 2:1 W/B",
+        //    "(W9863):Respite 1:1 Enhanced 15 min W/B",
+        //    "(W9862):Respite 15 min W/B",
+        //    "(W9862:U4):Respite 15 min W/O",
+        //    "(W9798):Respite 24HR W/B",
+        //    "(W9798:U4):Respite 24 HR W/O",
+        //    "(W7235):SE Career Assessment W/B",
+        //    "(W9794): SE Job Coach W/B",
+        //    "(W9794:U4):SE Job Coach W/O",
+        //    "(H2023):SE Job Find W/B"
+        //};
 
         public Workbook CreateWorkbook(UploadedFile uploadedNCSFile, UploadedFile uploadedSEVFile)
         {
@@ -119,8 +119,8 @@ namespace ConsumerMaster
                 } 
                 into tempJoin
                 from leftJoin in tempJoin.DefaultIfEmpty()
-                //orderby ca.Field<string>("Activity ID")
-                select new BillingCompareTransaction
+                orderby leftJoin?.Field<string>("service"), ncs.Field<string>("client_name")
+                 select new BillingCompareTransaction
                 {
                     NCSClientName = ncs.Field<string>("client_name"),
                     NCSStaffName = ncs.Field<string>("staff_name"),
@@ -129,62 +129,50 @@ namespace ConsumerMaster
                     NCSService = ncs.Field<string>("service"),
                     NCSWCode = ncs.Field<string>("wcode"),
                     NCSBaseWCode = ncs.Field<string>("base_wcode"),
-                    SEVClientName = leftJoin?.Field<string>("client_name"),
-                    SEVStaffName = leftJoin?.Field<string>("staff_name"),
-                    SEVStartDate = leftJoin?.Field<DateTime>("join_start_date"),
-                    SEVEndDate = leftJoin?.Field<DateTime>("join_end_date"),
                     SEVService = leftJoin?.Field<string>("service"),
-                    SEVWCode = leftJoin?.Field<string>("wcode")
+                    SEVWCode = leftJoin?.Field<string>("wcode"),
+
                 }).ToList();
 
-                DataTable billingTransactions = util.ConvertToDataTable(billingTransactionList);
+                DataTable compareTransactions = util.ConvertToDataTable(billingTransactionList);
 
-                foreach (DataRow row in billingTransactions.Rows)
+                int rowCount = Sheet1WorksheetHeader(sheet1Worksheet, compareTransactions);
+
+                int currentRow = IndexRowItemStart + rowCount;
+                foreach (DataRow row in compareTransactions.Rows)
                 {
-                    string str = row["NCSClientName"].ToString();
+                    int column = 0;
 
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["NCSClientName"].ToString());
+
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["NCSStaffName"].ToString());
+
+                    CellValueFormat dateTimeFormat = new CellValueFormat("MM/dd/yyyy hh:mm AM/PM");
+                    sheet1Worksheet.Cells[currentRow, column].SetFormat(dateTimeFormat);
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["NCSStartDate"].ToString());
+
+                    sheet1Worksheet.Cells[currentRow, column].SetFormat(dateTimeFormat);
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["NCSEndDate"].ToString());
+
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["NCSService"].ToString());
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["NCSWCode"].ToString());
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["NCSBaseWCode"].ToString());
+
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["SEVService"].ToString());
+                    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["SEVWCode"].ToString());
+
+                    if (!row["NCSBaseWCode"].ToString().Equals(row["SEVWCode"].ToString()))
+                    {
+                        sheet1Worksheet.Cells[currentRow, column++].SetValue("MISMATCHED");
+                    }
+
+                    currentRow++;
                 }
 
-                //int rowCount = rowCount = Sheet1WorksheetHeader(sheet1Worksheet, evvTransactions);
-
-                //int currentRow = IndexRowItemStart + rowCount;
-                //foreach (DataRow row in evvTransactions.Rows)
-                //{
-                //    int column = 0;
-
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["ActivityID"].ToString());
-
-                //    CellValueFormat textFormat = new CellValueFormat("@");
-                //    sheet1Worksheet.Cells[currentRow, column].SetFormat(textFormat);
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["ClientID"].ToString());
-
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["ClientName"].ToString());
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["StaffID"].ToString());
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["StaffName"].ToString());
-
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["ActivityType"].ToString());
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["ActivitySource"].ToString());
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["BillingCode"].ToString());
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["PayrollCode"].ToString());
-
-                //    CellValueFormat dateTimeFormat = new CellValueFormat("MM/dd/yyyy hh:mm AM/PM");
-                //    sheet1Worksheet.Cells[currentRow, column].SetFormat(dateTimeFormat);
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Start"].ToString());
-
-                //    sheet1Worksheet.Cells[currentRow, column].SetFormat(dateTimeFormat);
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Finish"].ToString());
-
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Duration"].ToString());
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Alerts"].ToString());
-                //    sheet1Worksheet.Cells[currentRow, column++].SetValue(row["Status"].ToString());
-
-                //    currentRow++;
-                //}
-
-                //for (int i = 1; i < sevDataTable.Columns.Count; i++)  //Start at 1 instead of 0
-                //{
-                //    sheet1Worksheet.Columns[i].AutoFitWidth();
-                //}
+                for (int i = 1; i < sevDataTable.Columns.Count; i++)  //Start at 1 instead of 0
+                {
+                    sheet1Worksheet.Columns[i].AutoFitWidth();
+                }
             }
             catch (Exception ex)
             {
@@ -202,29 +190,9 @@ namespace ConsumerMaster
                 worksheet.Cells[rowCount, 0].SetIsBold(true);
                 worksheet.Cells[rowCount++, 0].SetValue("AWC EVV Visits Comparison Report");
 
-
-               //String dtString = $"Date/time:{0}", DateTime.Now.ToString("MM/dd/yyyy hh:mm tt"));
-               //var dtString = string.Format("Date/time:{0:MM/dd/yyyy hh:mm tt}", DateTime.Now);
-               //var dtString1 = $"Date/time:{DateTime.Now:MM/dd/yyyy hh:mm tt}";
-
                 worksheet.Cells[rowCount++, 0].SetValue($"Date/time:{DateTime.Now:MM/dd/yyyy hh:mm tt}");
                 rowCount++;
 
-                //worksheet.Cells[rowCount, 0].SetIsBold(true);
-                //worksheet.Cells[rowCount, 0].SetValue("Visit Status");
-                //worksheet.Cells[rowCount, 1].SetIsBold(true);
-                //worksheet.Cells[rowCount++, 1].SetValue("Visit Description");
-
-                //worksheet.Cells[rowCount, 0].SetValue("In Process");
-                //worksheet.Cells[rowCount++, 1].SetValue("A visit has started and not yet completed");
-                //worksheet.Cells[rowCount, 0].SetValue("Incomplete");
-                //worksheet.Cells[rowCount++, 1].SetValue("A visit has exceeded a 24-hr. period and is still missing a call-in/call-out");
-                //worksheet.Cells[rowCount, 0].SetValue("Verified");
-                //worksheet.Cells[rowCount++, 1].SetValue("A visit that does not contain any exceptions");
-                //worksheet.Cells[rowCount, 0].SetValue("Processed");
-                //worksheet.Cells[rowCount++, 1].SetValue("A visit that does not contain any exceptions and has been returned to the claims validation engine at least once");
-                //worksheet.Cells[rowCount, 0].SetValue("Omit");
-                //worksheet.Cells[rowCount++, 1].SetValue("A visit that is marked \"Do Not Bill\"");
                 rowCount++;
 
                 string[] columnNames = dataTable.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToArray();
@@ -254,17 +222,17 @@ namespace ConsumerMaster
 
         public class BillingCompareTransaction
         {
-            public string NCSClientName { get; set; } 
+            public string NCSClientName { get; set; }
             public string NCSStaffName { get; set; }
             public DateTime? NCSStartDate { get; set; }
             public DateTime? NCSEndDate { get; set; }
             public string NCSService { get; set; }
             public string NCSWCode { get; set; }
             public string NCSBaseWCode { get; set; }
-            public string SEVClientName { get; set; }
-            public string SEVStaffName { get; set; }
-            public DateTime? SEVStartDate { get; set; }
-            public DateTime? SEVEndDate { get; set; }
+            //public string SEVClientName { get; set; }
+            //public string SEVStaffName { get; set; }
+            //public DateTime? SEVStartDate { get; set; }
+            //public DateTime? SEVEndDate { get; set; }
             public string SEVService { get; set; }
             public string SEVWCode { get; set; }
         }
